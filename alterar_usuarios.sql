3f8250d164bc98b5b8eb100249b5c12622e69493
MaCDreikeR <borgespereira87@gmail.com>
2025-09-15 01:30:34 -0300

    arquivos deletados

--- alterar_usuarios.sql
+++
@@ -1,7 +0,0 @@
--- Adiciona a coluna foto_url à tabela usuarios
-ALTER TABLE usuarios
-ADD COLUMN IF NOT EXISTS foto_url TEXT;
-
--- Atualiza a coluna foto_url para permitir armazenamento de URLs longas
-ALTER TABLE usuarios
-ALTER COLUMN foto_url TYPE TEXT; 
